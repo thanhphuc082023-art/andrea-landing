cb0f4057402319e5b516123e238ad127c7e865b3
Minh Huy <citythree.11798@gmail.com>
2025-08-13 21:28:53 +0700

    feat: Update project details and add new projects
    - Replaced existing project details for HOROLUX and DECOFI with updated information and images.
    - Introduced new projects: HUDECO, HDCS, and CUDOHA, enhancing the project portfolio.
    - Adjusted project slugs and descriptions for better clarity and representation.

--- apps/fe/src/data/projects.ts
+++ apps/fe/src/data/projects.ts
@@ -4,50 +4,51 @@ export const projects: Project[] = [
   // Featured Projects
   {
     id: 1,
-    title: 'HOROLUX',
-    description:
-      'Logo, Bộ nhận diện thương hiệu, Social branding, Thiết kế bao bì',
-    image: '/assets/images/featured-projects/horolux.jpg',
-    isFeatured: true,
-    isLarge: true,
-    slug: 'horolux',
-  },
-  {
-    id: 2,
     title: 'BĂNG RỪNG NGẬP MẶN',
     description:
       'Logo, Bộ nhận diện thương hiệu, Social branding, Thiết kế bao bì',
     image: '/assets/images/featured-projects/rungngapman.jpg',
     isFeatured: true,
-    isLarge: false,
+    isLarge: true,
     slug: 'rungngapman',
   },
-  // Regular Projects
   {
-    id: 3,
+    id: 2,
     title: 'DECOFI',
     description:
       'Logo, Bộ nhận diện thương hiệu, Profile, Website, Hệ thống chỉ dẫn công trường, Báo cáo thường niên, Thiết kế nhận diện sự kiện',
     image: '/assets/images/featured-projects/decofi-catalog.jpg',
-    isFeatured: false,
+    isFeatured: true,
     slug: 'decofi',
   },
+
+  // Regular Projects
   {
-    id: 4,
-    title: 'HSME',
-    description: 'Logo, Bộ nhận diện thương hiệu, Profile, ',
-    image: '/assets/images/featured-projects/hsme-catalog.jpg',
-    isFeatured: false,
-    slug: 'hsme',
-  },
-  {
-    id: 5,
+    id: 3,
     title: 'MOBIFONE',
     description: 'Thiết kế nhận diện sự kiện, Thiết kế bao bì',
     image: '/assets/images/featured-projects/mobifone.jpg',
     isFeatured: false,
     slug: 'mobifone',
   },
+  {
+    id: 4,
+    title: 'MITSUBISHI',
+    description:
+      'Logo, Bộ nhận diện thương hiệu, Social branding, Thiết kế bao bì',
+    image: '/assets/images/featured-projects/mitsubishi.jpg',
+    isFeatured: false,
+    slug: 'mitsubishi',
+  },
+  {
+    id: 5,
+    title: 'HSME',
+    description: 'Logo, Bộ nhận diện thương hiệu, Profile, ',
+    image: '/assets/images/featured-projects/hsme-catalog.jpg',
+    isFeatured: false,
+    slug: 'hsme',
+  },
+
   {
     id: 6,
     title: 'DELAFÉE',
@@ -67,12 +68,39 @@ export const projects: Project[] = [
   },
   {
     id: 8,
-    title: 'MITSUBISHI',
+    title: 'HOROLUX',
     description:
       'Logo, Bộ nhận diện thương hiệu, Social branding, Thiết kế bao bì',
-    image: '/assets/images/featured-projects/mitsubishi.jpg',
+    image: '/assets/images/featured-projects/horolux.jpg',
     isFeatured: false,
-    slug: 'mitsubishi',
+    slug: 'horolux',
+  },
+  {
+    id: 9,
+    title: 'HUDECO',
+    description:
+      'Logo, Bộ nhận diện thương hiệu, Social branding, Thiết kế bao bì',
+    image: '/assets/images/projects/project-sample.jpg',
+    isFeatured: false,
+    slug: 'hudeco',
+  },
+  {
+    id: 10,
+    title: 'HDCS',
+    description:
+      'Logo, Bộ nhận diện thương hiệu, Social branding, Thiết kế bao bì',
+    image: '/assets/images/projects/project-sample.jpg',
+    isFeatured: false,
+    slug: 'hdcs',
+  },
+  {
+    id: 11,
+    title: 'CUDOHA',
+    description:
+      'Logo, Bộ nhận diện thương hiệu, Social branding, Thiết kế bao bì',
+    image: '/assets/images/projects/project-sample.jpg',
+    isFeatured: false,
+    slug: 'cudoha',
   },
 ];
 
