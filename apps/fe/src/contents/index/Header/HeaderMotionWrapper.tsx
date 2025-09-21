00dcad8c2f59b50f9d04704048e506bc07e4b938
Minh Huy <citythree.11798@gmail.com>
2025-09-22 00:51:09 +0700

    Refactor HeaderMotion component to use clsx for dynamic class handling
    - Update HeaderMotion to conditionally apply height classes based on animation completion using clsx.
    - Remove dynamic import of HeaderMotion in HeaderMotionWrapper for a more straightforward import approach.

--- apps/fe/src/contents/index/Header/HeaderMotionWrapper.tsx
+++ apps/fe/src/contents/index/Header/HeaderMotionWrapper.tsx
@@ -4,7 +4,10 @@ import dynamic from 'next/dynamic';
 import React from 'react';
 
 // Dynamically import HeaderMotion with SSR disabled
-const HeaderMotion = dynamic(() => import('./HeaderMotion'), { ssr: false });
+const HeaderMotion = dynamic(() => import('./HeaderMotion'), {
+  ssr: false,
+  loading: () => <div className="flex h-screen" />,
+});
 
 interface Props {
   heroData?: any;
